273853a4117449f03d1b9614fc6721572375645c
Lu-weibin <13106658600@163.com>
2019-04-02 15:30:55 +0800

    edit on hot-fix

--- src/main/java/com/wei/git/TestGit.java
+++ src/main/java/com/wei/git/TestGit.java
@@ -3,5 +3,7 @@ package com.wei.git;
 public class TestGit {
 	public static void main(String[] args) {
 		System.out.println("test Git Right");		System.out.println("add test Git Left");
+		System.out.println("hot-fix");
+		
 	}
 }
